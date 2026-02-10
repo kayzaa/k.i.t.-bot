be777565f01a9f0fdf223db16f09fdea1b1393a0
K.I.T. [Translator] <kit-bot@binaryfaster.com>
2026-02-11 00:14:31 +0100

    feat: add DeBank DeFi portfolio aggregator skill

---
+++ skills/debank-aggregator/index.ts
@@ -0,0 +1,7 @@
+/**
+ * DeBank DeFi Portfolio Aggregator
+ * K.I.T. Skill - Track DeFi positions across 1300+ protocols
+ */
+
+export * from './debank-client';
+export { DeBankClient as default } from './debank-client';
